625630e560daf616339749c1a2c8b56c60a991bf
Trevor Low <tlow94@uw.edu>
2020-03-30 22:23:02 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,7 +1,10 @@
 # don't forget to add: require 'pry'
-
+require 'pry'
 def start_game(player1, player2)
+  player1 = "Rocky"
+  player2 = "Bullwinkle"
   "Hello #{player1} & #{playar2}"
+  binding.pry
 end
 
 def play_game(player2)
