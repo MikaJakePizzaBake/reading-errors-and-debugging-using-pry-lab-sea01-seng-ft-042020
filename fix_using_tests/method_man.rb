9f4306d78e3dd129a9a13d89e690bd9e40344157
Trevor Low <tlow94@uw.edu>
2020-03-30 22:24:33 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,12 +1,8 @@
 # don't forget to add: require 'pry'
-require 'pry'
 def start_game(player1, player2)
-  player1 = "Rocky"
-  player2 = "Bullwinkle"
-  "Hello #{player1} & #{playar2}"
-  binding.pry
+  "Hello #{player1} & #{player2}"
 end
 
-def play_game(player2)
+def play_game(player1, player2)
   "#{player1} is better than #{player2}"
 end
